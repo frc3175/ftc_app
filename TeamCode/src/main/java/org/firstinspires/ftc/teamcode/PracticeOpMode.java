2d49f709bc0ee91a5e21a77b8fbfd598e56132d3
tonypan2000 <tonypan2000@gmail.com>
2017-10-21 15:42:23 -0400

    Added movement, turns & telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeOpMode.java
@@ -3,43 +3,83 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import static android.R.attr.left;
-import static android.R.attr.right;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by tonyp on 10/21/2017.
  */
 @TeleOp (name = "PracticeOpMode")
 public class PracticeOpMode extends OpMode{
-    private DcMotor leftDrive = null; //use left stick to go forward/back, use right stick to turn
-    private DcMotor rightDrive = null;
-    private DcMotor strafeDrive = null; //use bars
-    private static final double TURN_POWER = 0.5
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null; //use left stick to go forward/back, use right stick to turn
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor strafeFrontDrive = null; //use bars
+    private DcMotor strafeBackDrive = null;
+    private static final double TURN_POWER = 0.5;//sets constant for turn power
+    private static final double STRAFE_POWER = 0.5;//sets constant for strafe power
+
     @Override
     public void init() {
-        leftDrive = hardwareMap.get(DcMotor.class, "LeftDrive");
-        rightDrive = hardwareMap.get(DcMotor.class, "RightDrive");
-        strafeDrive = hardwareMap.get(DcMotor.class, "StrafeDrive");
+        telemetry.addData("Status", "Initializing");//tells the user(s) that the robot is initializing
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "LeftFrontDrive"); //initializes motors
+        leftBackDrive = hardwareMap.get(DcMotor.class, "LeftBackDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "RightFrontDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "RightBackDrive");
+        strafeFrontDrive = hardwareMap.get(DcMotor.class, "StrafeFrontDrive");
+        strafeBackDrive = hardwareMap.get(DcMotor.class, "StrafeBackDrive");
 
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
         //reverses direction of right motors, change if the direction is wrong
+        telemetry.addData("Status", "Initialized"); //tells the user(s) init. is finished
     }
 
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
 
     @Override
     public void loop() {
-        double leftTurn = gamepad1.right_stick_x;
-        leftDrive.setPower(-gamepad1.left_stick_y); //sets left stick to fwd drive
-        rightDrive.setPower(-gamepad1.left_stick_y);
+        double leftPower = -gamepad1.left_stick_y;//defines variables used to move up&down
+        double rightPower = -gamepad1.left_stick_y;
+        leftPower += gamepad1.right_stick_x * TURN_POWER; //enables turns
+        rightPower -= gamepad1.right_stick_x * TURN_POWER;//uses right stick left&right
+        leftFrontDrive.setPower(leftPower);//enables forward&backward movement
+        leftBackDrive.setPower(leftPower);//uses left stick up&down
+        rightFrontDrive.setPower(rightPower);
+        rightBackDrive.setPower(rightPower);
         if (gamepad1.left_bumper) {
-            strafeDrive.setPower(0.5); //makes left bumper set strafe motors to positive
-
-            if (gamepad1.right_bumper) {
-                strafeDrive.setPower(-0.5); //makes right bumper set strafe motors to negative
+            strafeFrontDrive.setPower(STRAFE_POWER);//makes left bumper set strafe motors to positive
+            strafeBackDrive.setPower(STRAFE_POWER);
         }
-
-
+        if (gamepad1.right_bumper) {
+            strafeFrontDrive.setPower(-STRAFE_POWER); //makes right bumper set strafe motors to negative
+            strafeBackDrive.setPower(-STRAFE_POWER);
         }
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
 }

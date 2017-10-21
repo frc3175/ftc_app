c69d9974a506e413bd35705178cd9d782d161ee2
tonypan2000 <tonypan2000@gmail.com>
2017-10-21 12:50:48 -0400

    Created PracticeOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeOpMode.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by tonyp on 10/21/2017.
+ */
+@TeleOp (name = "PracticeOpMode")
+public class PracticeOpMode extends OpMode{
+    private 
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
